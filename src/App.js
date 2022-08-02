e0c9f59c437f292b2c127315ab2c46b0dd3cf446
antoniehuang <antonieyuchi@gmail.com>
2022-08-02 08:14:10 +0100

    Added the about me page using react-bootstrap

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* React Imports */
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" expand='lg'>
+        <Container>
+          <Navbar.Brand href="#">Antonie</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container id='about-me' className='shadow-lg rounded p-3 m-6'>
+        <div className='post m-3'>
+          <Row>
+            <Col>
+              <h1>Antonie (Yu-Chi) Huang</h1>
+              <p>Full Stack Deep Learning Developer</p>
+            </Col>
+            <Row>
+              <Col>
+                <article>
+                  <p>Ooh Hello! My name is Yu-Chi Huang, but I go by Antonie most of the time.</p>
+                  <p>I recently graduated from The University of Birmingham with a degree in Artificial
+                    Intelligence
+                    and
+                    Computer
+                    Science. My interest in AI focuses on thinking about how computers can simulate
+                    imaginations,
+                    generate
+                    synthetic samples and the ways to use simulations and synthetic samples to better train our
+                    AI
+                    agents.
+                  </p>
+                  <p>You can connect with me by clicking the icons below!</p>
+                </article>
+              </Col>
+              <Col className='col-md-auto d-flex justify-content-center'>
+                <Image src="img/prof_pic.jpeg" id="prof-pic" className='z-depth-1' fluid rounded
+                  alt="An image of myself with a tucan!"></Image>
+              </Col>
+            </Row>
+          </Row>
+        </div>
+      </Container >
+    </div >
+  );
+}
+
+export default App;
